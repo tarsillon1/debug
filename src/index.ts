bf0cb95b5fb0417ab1716a24b8ed979d604d02e9
tarsillon1o <tarsillon1@gmail.com>
2020-03-25 14:59:05 -0400

    shitty code

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,142 @@
-import * as dotenv from "dotenv";
+var net = require("net");
 
-import * as fs from "fs";
-import * as http from "http";
-import * as https from "https";
-import * as express from "express";
-import * as bodyParser from "body-parser";
+// creates the server
+var server = net.createServer();
 
-// Init the server.
-async function init(): Promise<http.Server> {
-  const app = express();
-  app.use(bodyParser.json());
+//emitted when server closes ...not emitted until all connections closes.
+server.on("close", function() {
+  console.log("Server closed !");
+});
 
-  // Setup statuc assets.
-  app.use(express.static("public"));
+// emitted when new client connects
+server.on("connection", function(socket: any) {
+  //this property shows the number of characters currently buffered to be written. (Number of characters is approximately equal to the number of bytes to be written, but the buffer may contain strings, and the strings are lazily encoded, so the exact number of bytes is not known.)
+  //Users who experience large or growing bufferSize should attempt to "throttle" the data flows in their program with pause() and resume().
 
-  // Setup routes.
-  app.use((req: express.Request, res: express.Response) => {
-    console.log("headers:", req.headers);
-    console.log("body:", req.body);
-    res.status(200).send("OK");
+  console.log("Buffer size : " + socket.bufferSize);
+
+  console.log("---------server details -----------------");
+
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log("Server is listening at port" + port);
+  console.log("Server ip :" + ipaddr);
+  console.log("Server is IP4/IP6 : " + family);
+
+  var lport = socket.localPort;
+  var laddr = socket.localAddress;
+  console.log("Server is listening at LOCAL port" + lport);
+  console.log("Server LOCAL ip :" + laddr);
+
+  console.log("------------remote client info --------------");
+
+  var rport = socket.remotePort;
+  var raddr = socket.remoteAddress;
+  var rfamily = socket.remoteFamily;
+
+  console.log("REMOTE Socket is listening at port" + rport);
+  console.log("REMOTE Socket ip :" + raddr);
+  console.log("REMOTE Socket is IP4/IP6 : " + rfamily);
+
+  console.log("--------------------------------------------");
+  //var no_of_connections =  server.getConnections(); // sychronous version
+  server.getConnections(function(error: any, count: any) {
+    console.log("Number of concurrent connections to the server : " + count);
   });
-  return https.createServer(
-    {
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert")
-    },
-    app
-  );
+
+  socket.setEncoding("utf8");
+
+  socket.setTimeout(800000, function() {
+    // called after timeout -> same as socket.on('timeout')
+    // it just tells that soket timed out => its ur job to end or destroy the socket.
+    // socket.end() vs socket.destroy() => end allows us to send final data and allows some i/o activity to finish before destroying the socket
+    // whereas destroy kills the socket immediately irrespective of whether any i/o operation is goin on or not...force destry takes place
+    console.log("Socket timed out");
+  });
+
+  socket.on("data", function(data: any) {
+    var bread = socket.bytesRead;
+    var bwrite = socket.bytesWritten;
+    console.log("Bytes read : " + bread);
+    console.log("Bytes written : " + bwrite);
+    console.log("Data sent to server : " + data);
+
+    //echo data
+    var is_kernel_buffer_full = socket.write("Data ::" + data);
+    if (is_kernel_buffer_full) {
+      console.log(
+        "Data was flushed successfully from kernel buffer i.e written successfully!"
+      );
+    } else {
+      socket.pause();
+    }
+  });
+
+  socket.on("drain", function() {
+    console.log(
+      "write buffer is empty now .. u can resume the writable stream"
+    );
+    socket.resume();
+  });
+
+  socket.on("error", function(error: any) {
+    console.log("Error : " + error);
+  });
+
+  socket.on("timeout", function() {
+    console.log("Socket timed out !");
+    socket.end("Timed out!");
+    // can call socket.destroy() here too.
+  });
+
+  socket.on("end", function(data: any) {
+    console.log("Socket ended from other end!");
+    console.log("End data : " + data);
+  });
+
+  socket.on("close", function(error: any) {
+    var bread = socket.bytesRead;
+    var bwrite = socket.bytesWritten;
+    console.log("Bytes read : " + bread);
+    console.log("Bytes written : " + bwrite);
+    console.log("Socket closed!");
+    if (error) {
+      console.log("Socket was closed coz of transmission error");
+    }
+  });
+
+  setTimeout(function() {
+    var isdestroyed = socket.destroyed;
+    console.log("Socket destroyed:" + isdestroyed);
+    socket.destroy();
+  }, 1200000);
+});
+
+// emits when any error occurs -> calls closed event immediately after this.
+server.on("error", function(error: any) {
+  console.log("Error: " + error);
+});
+
+//emits when server is bound with server.listen
+server.on("listening", function() {
+  console.log("Server is listening!");
+});
+
+server.maxConnections = 10;
+
+//static port allocation
+server.listen(5228);
+
+var islistening = server.listening;
+
+if (islistening) {
+  console.log("Server is listening");
+} else {
+  console.log("Server is not listening");
 }
 
-if (require.main === module) {
-  dotenv.config();
-  init()
-    .then(app => {
-      const port = process.env.PORT || 5228;
-      app.listen(port);
-      console.log("server listening on", port);
-    })
-    .catch(console.error);
-}
+setTimeout(function() {
+  server.close();
+}, 5000000);
